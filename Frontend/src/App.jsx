7e067d4b6cb0ff4956c8983d3ac7920ad0f94b14
yuan-latisa <email_primary_yuann1423@gmail.com>
2025-11-23 22:14:55 +0700

    reorganize

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* halaman awal */}
+        <Route path="/" element={<Register />} />
+
+        {/* halaman lain */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+
+        {/* Fallback untuk semua URL yang tidak ada */}
+        <Route path="*" element={<Register />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,3 @@
+export default function Register() {
+  return <div>Halaman Daftar</div>;
+}
